dc6ba646d81385ea7fdaba212b10ed92553007be
pavlyuchenkov_aa <a-pavlyuchenkov@bk.ru>
2022-12-15 15:27:15 +0300

    Added view for company and product filtering for data graph

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -130,6 +130,109 @@ const Header = () => {
         >
           <SettingsIcon />
         </IconButton>
+        <Dialog open={open} onClose={handleClose}>
+          <DialogTitle>{daliogTitle}</DialogTitle>
+          <DialogContent>
+            <Box sx={{ width: 450, typography: 'body1' }}>
+              <TabContext value={tabVal}>
+                <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                  <TabList onChange={handleTabChange} aria-label="lab API tabs example">
+                    <Tab label="Компании" value="Фильтрация компаний" />
+                    <Tab label="Продукты и релизы" value="Фильтрация продуктов" />
+                  </TabList>
+                </Box>
+                <TabPanel value="Фильтрация компаний">
+                  <FormControl sx={{ m: 1, ml: -3, minWidth: 450 }}>
+                    <InputLabel id="demo-simple-select-autowidth-label">Наименование компании</InputLabel>
+                    <Select
+                      labelId="demo-simple-select-autowidth-label"
+                      id="demo-simple-select-autowidth"
+                      label="Наименование компании"
+                    >
+                      <MenuItem value={10}>Компания А</MenuItem>
+                      <MenuItem value={21}>Компания Б</MenuItem>
+                    </Select>
+                  </FormControl>
+                  <FormControl sx={{ m: 1, ml: -3, minWidth: 450 }}>
+                    <InputLabel id="demo-simple-select-autowidth-label">Имя владельца</InputLabel>
+                    <Select
+                      labelId="demo-simple-select-autowidth-label"
+                      id="demo-simple-select-autowidth"
+                      label="Имя владельца"
+                    >
+                      <MenuItem value={10}>Владелец А</MenuItem>
+                      <MenuItem value={21}>Владелец Б</MenuItem>
+                    </Select>
+                  </FormControl>
+                  <Box sx={{ m: 1, ml: -3, width: 450 }}>
+                    <Typography id="track-false-slider" gutterBottom>
+                      Штат сотрудников
+                    </Typography>
+                    <Slider
+                      aria-labelledby="track-false-slider"
+                      defaultValue={[20, 37]}
+                      marks={marks}
+                    />
+                    <Separator />
+                    <Typography id="track-false-range-slider" gutterBottom>
+                      Время существования
+                    </Typography>
+                    <Slider
+                      aria-labelledby="track-false-range-slider"
+                      defaultValue={[20, 37]}
+                      marks={yearMarks}
+                    />
+                  </Box>
+                  <FormControl sx={{ m: 1, ml: -3, minWidth: 450 }}>
+                    <InputLabel id="demo-simple-select-autowidth-label">Отрасли</InputLabel>
+                    <Select
+                      labelId="demo-simple-select-autowidth-label"
+                      id="demo-simple-select-autowidth"
+                      label="Отрасли"
+                    >
+                      <MenuItem value={10}>Отрасль 1</MenuItem>
+                      <MenuItem value={21}>Отрасль 2</MenuItem>
+                    </Select>
+                  </FormControl>
+                  <Separator />
+                  <Box sx={{ m: 1, ml: -3, width: 450 }}>
+                    <Button variant="contained">Применить</Button>
+                    <Button variant="text">Сбросить</Button>
+                  </Box>
+                </TabPanel>
+                <TabPanel value="Фильтрация продуктов">
+                  <FormControl sx={{ m: 1, ml: -3, minWidth: 450 }}>
+                    <InputLabel id="demo-simple-select-autowidth-label">Наименование продукта</InputLabel>
+                    <Select
+                      labelId="demo-simple-select-autowidth-label"
+                      id="demo-simple-select-autowidth"
+                      label="Наименование продукта"
+                    >
+                      <MenuItem value={10}>Продукт А</MenuItem>
+                      <MenuItem value={21}>Продукт Б</MenuItem>
+                    </Select>
+                  </FormControl>
+                  <Box sx={{ m: 1, ml: -3, width: 450 }}>
+                    <Typography id="track-false-range-slider" gutterBottom>
+                      Время существования
+                    </Typography>
+                    <Slider
+                      aria-labelledby="track-false-range-slider"
+                      defaultValue={[20, 37]}
+                      marks={yearMarks}
+                    />
+                    <FormGroup>
+                    <FormControlLabel control={<Checkbox/>} label="Релиз подтвержден" />
+                    </FormGroup>
+                    <Separator />
+                    <Button variant="contained">Применить</Button>
+                    <Button variant="text">Сбросить</Button>
+                  </Box>
+                </TabPanel>
+              </TabContext>
+            </Box>
+          </DialogContent>
+        </Dialog>
       </Toolbar>
     </AppBar>
   )
